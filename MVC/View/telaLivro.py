f8e21548ffde97f7bcb775983bd343b50c1727c9
Manu <55411177+ManuFoscarini@users.noreply.github.com>
2021-04-20 14:47:51 -0300

    Update telaLivro.py

--- MVC/View/telaLivro.py
+++ MVC/View/telaLivro.py
@@ -8,19 +8,11 @@ class TelaLivro(Tela):
 
     def tela_opcoes(self):
         print("-------- LIVROS ----------")
-        print("1 - Incluir livro")
-        print("2 - Alterar livro")
-        print("3 - Mostrar livro")
+        print("1 - Mostrar livro")
         print("0 - Retornar")
 
         opcao = int(input("Escolha a opcao: "))
         return opcao
-
-    #sem função no código
-    def pega_autor(self,autor):
-        autor = input("Autor: ")
-
-        return{"Autor", autor}
 
     def pega_titulo(self, tipo: str):
         continua = True
@@ -41,9 +33,11 @@ class TelaLivro(Tela):
                 'Editora': editora}
 
     def mostra_livro(self, dados_livro):
+        print('-------------------------------------------------')
         print("Nome do livro: %s" % (dados_livro["Titulo"]))
         print("Autor do livro: %s" % (dados_livro["Autor"]))
         print("Editora do livro: %s" % (dados_livro['Editora']))
-
+        print('-------------------------------------------------')
+        print('\n')
 
 
